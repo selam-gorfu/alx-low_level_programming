3096fbdea3c6cd98c7c3db08cea2aa642153c498
selam-gorfu <princesssely@gmail.com>
2023-09-14 11:55:46 -0700

    writing a statement using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main entry
+ *
+ * return: 0 if success
+ */
+
+int main(void)
+{
+	char statement[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(statement);
+
+	return (0);
+}	
