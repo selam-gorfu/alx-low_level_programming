6964e1609f5455b118d93acf9c02a177eaf18555
selam-gorfu <princesssely@gmail.com>
2023-09-24 21:24:07 -0700

    swaps two numbers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps two integers
+ *
+ * @a: integer 1 
+ * @b: integer 2 
+ *
+ * return: Always 0 (success)
+ */
+
+void swap_int(int *a, int *b)
+{
+	int swap;
+
+	swap = *a;
+	*a = *b;
+	*b = swap;
+}
