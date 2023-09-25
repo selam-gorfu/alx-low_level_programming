2b27c89d28a54eefbfdeee93ffbaa315ad08d479
selam-gorfu <princesssely@gmail.com>
2023-09-24 20:47:00 -0700

    testing whether it prints positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - tests if it prints a number is poitive or negative
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
