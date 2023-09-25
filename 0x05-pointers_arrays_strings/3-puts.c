4a803eaf8f72577c1dc5c0837ea47df3fb9319d8
selam-gorfu <princesssely@gmail.com>
2023-09-24 21:35:15 -0700

    prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ *
+ * @str: printed  string 
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+void _puts(char *str)
+{
+	char *r = str;
+
+	while (*r != '\0')
+	{
+		_putchar(*r);
+
+		r++;
+	}
+
+	_putchar('\n');
+}
