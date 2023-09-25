cd4fbc05e24825821a5c4536822e114f0644c623
selam-gorfu <princesssely@gmail.com>
2023-09-24 21:30:03 -0700

    counts a string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of a string
+ *
+ * @s:  a string measured
+ *
+ * Return: Always 0 'Success'
+ */
+
+int _strlen(char *s)
+{
+	int z, len = 0;
+
+	for (i = 0; s[z] != '\0'; z++)
+	{
+		len++;
+	}
+
+	return (len);
+}
